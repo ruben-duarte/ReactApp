92380d053f9794db51ca7b4ecea201fc9d46e908
Ruben Dario Duarte Anaya <ruben.duarte.anaya@gmail.com>
2024-03-26 12:35:27 -0500

    styles h1 and input, fetch to the api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import  { useEffect } from 'react';
+import './App.css';
+import searchIcon from './search.svg';
+
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=3c987d8d'; 
 
 const App = () => {
+    const searchMovie = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovie('star wars');
+    }, []);
+    
     return(
-        <h1>App React</h1>     
+        <div className='app'>
+            <h1>Tus peliculas favoritas 🎬</h1>
+            <div className='search'>
+                <input 
+                    placeholder='busca tus peliculas aqui'
+                />
+            </div>
+        </div>
     );
 }
 
